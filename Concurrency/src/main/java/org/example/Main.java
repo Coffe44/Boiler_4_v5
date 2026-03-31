8996c5ab7a8f05c3ceac52a9e540ede43d531dad
Felix Liden <>
2026-03-31 08:54:33 +0200

    del 2.1: Added visibility to threading

--- Concurrency/src/main/java/org/example/Main.java
+++ Concurrency/src/main/java/org/example/Main.java
@@ -6,11 +6,14 @@ public class Main {
 
         for (int i = 0; i < 100; i++)
         {
+            int x = i;
             new Thread(() ->
             {
                 p.takeDamage(10);
+                System.out.println("X " + x);
             }).start();
 
+            System.out.println("loop " + i);
         }
 
         Thread.sleep(1000);
