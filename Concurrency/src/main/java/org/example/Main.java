59b7c2eb1356a6bda8d1b56c20d7f2a50c9e7a66
Felix Liden <>
2026-03-31 10:34:26 +0200

    del 10: Added Future, not sure why

--- Concurrency/src/main/java/org/example/Main.java
+++ Concurrency/src/main/java/org/example/Main.java
@@ -1,28 +1,48 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.*;
 
 public class Main {
     static void main() throws InterruptedException
     {
-        int loops = 100;
+        int loops = 10;
 
         Player p = new Player("Boss");
         AttackLog l = new AttackLog();
 
         ExecutorService pool = Executors.newFixedThreadPool(5);
 
+        List<Future<Integer>> results = new ArrayList<>();
+
         for (int i = 0; i < loops; i++)
         {
-            int id = i;
-            pool.submit(() -> l.log("Attack " + id));
+            results.add
+                    (
+                            pool.submit(() ->
+                            {
+                                Thread.sleep(100);
+                                return 10;
+                            })
+                    );
+        }
+
+        int totalDamage = 0;
+
+        for (Future<Integer> future : results)
+        {
+            try
+            {
+                totalDamage += future.get();
+                } catch (Exception e)
+            {
+                e.printStackTrace();
+            }
         }
 
         pool.shutdown();
-        pool.awaitTermination(5, TimeUnit.SECONDS);
 
-        System.out.println("expected " + loops);
-        System.out.println("actual " + l.getAttacks().size());
-        System.out.println(l.getAttacks());
+        System.out.println("Total damage " + totalDamage);
     }
 }
