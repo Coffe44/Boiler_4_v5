2bd066c6485209f8dfb596afbfbe47aac1556622
Felix Liden <>
2026-03-31 09:10:57 +0200

    del 5: Changed Player health logic

--- Concurrency/src/main/java/org/example/Player.java
+++ Concurrency/src/main/java/org/example/Player.java
@@ -1,22 +1,31 @@
 package org.example;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Player
 {
     private String name;
-    private int health = 100;
+//    private int health = 100;
+private AtomicInteger health = new AtomicInteger(100);
+
 
     public Player(String name)
     {
         this.name = name;
     }
 
-    public synchronized void takeDamage(int damage)
+//    public synchronized void takeDamage(int damage)
+//    {
+//        int currentHealth = health;
+//
+//        try { Thread.sleep(1); } catch (Exception _){}
+//
+//        health = currentHealth - damage;
+//    }
+
+    public void takeDamage(int damage)
     {
-        int currentHealth = health;
-
-        try { Thread.sleep(1); } catch (Exception _){}
-
-        health = currentHealth - damage;
+        health.addAndGet(-damage);
     }
 
     public  String getName()
@@ -25,6 +34,6 @@ public class Player
     }
     public int getHealth()
     {
-        return health;
+        return health.get();
     }
 }
